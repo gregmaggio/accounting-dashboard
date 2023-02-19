cdce06d92231fd7b6242dd8272670fadce85b026
Gregory Maggio <gregorymaggio@gmail.com>
2023-02-19 14:33:52 -0500

    Use google app engine query (hee hee) service

--- src/app/service/accounting.service.ts
+++ src/app/service/accounting.service.ts
@@ -31,27 +31,27 @@ export class AccountingService {
     }
 
     min(): Observable<string> {
-        var uri: string = "https://datamagic.ca/Accounting/api/min";
+        var uri: string = " /api/min";
         return this.http.get<string>(uri);
     }
 
     max(): Observable<string> {
-        var uri: string = "https://datamagic.ca/Accounting/api/max";
+        var uri: string = "https://query-dot-api-project-378578942759.ue.r.appspot.com/api/max";
         return this.http.get<string>(uri);
     }
 
     eventNames(): Observable<EventName[]> {
-        var uri: string = "https://datamagic.ca/Accounting/api/eventNames";
+        var uri: string = "https://query-dot-api-project-378578942759.ue.r.appspot.com/api/eventNames";
         return this.http.get<EventName[]>(uri);
     }
 
     events(startYear:number, startMonth:number, startDay:number, endYear:number, endMonth:number, endDay:number): Observable<Event[]> {
-        var uri: string = "https://datamagic.ca/Accounting/api/stats/" + startYear.toString() + "/" + startMonth.toString() + "/" + startDay.toString() + "/" + endYear.toString() + "/" + endMonth.toString() + "/" + endDay.toString();
+        var uri: string = "https://query-dot-api-project-378578942759.ue.r.appspot.com/api/stats/" + startYear.toString() + "/" + startMonth.toString() + "/" + startDay.toString() + "/" + endYear.toString() + "/" + endMonth.toString() + "/" + endDay.toString();
         return this.http.get<Event[]>(uri);
     }
 
     trend(startYear:number, startMonth:number, startDay:number, endYear:number, endMonth:number, endDay:number, eventName:string, eventMessage:string): Observable<Trend> {
-        var uri: string = "https://datamagic.ca/Accounting/api/trend/" + startYear.toString() + "/" + startMonth.toString() + "/" + startDay.toString() + "/" + endYear.toString() + "/" + endMonth.toString() + "/" + endDay.toString() + "/" + eventName + "/" + eventMessage;
+        var uri: string = "https://query-dot-api-project-378578942759.ue.r.appspot.com/api/trend/" + startYear.toString() + "/" + startMonth.toString() + "/" + startDay.toString() + "/" + endYear.toString() + "/" + endMonth.toString() + "/" + endDay.toString() + "/" + eventName + "/" + eventMessage;
         return this.http.get<Trend>(uri);
     }
 }
